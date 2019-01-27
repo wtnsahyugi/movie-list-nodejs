b5c5878d8af6cfccb4d94477727e03932d983688
wtnsahyugi@gmail.com <wtnsahyugi@gmail.com>
2019-01-27 17:32:01 +0700

    test

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 require('dotenv').config();
+var path = require('path');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 const port = process.env.PORT || 5000;
@@ -8,6 +9,9 @@ const port = process.env.PORT || 5000;
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 app.use(require('./routes'));
 app.listen(port);
