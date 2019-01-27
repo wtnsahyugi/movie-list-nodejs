767fbc24e6296a1f98f3ec3c5a6eb3045bd90a14
wtnsahyugi@gmail.com <wtnsahyugi@gmail.com>
2019-01-27 19:08:17 +0700

    rebuild

--- frontend/src/modules/movie-browser/movie-browser.service.js
+++ frontend/src/modules/movie-browser/movie-browser.service.js
@@ -1,11 +1,9 @@
 // WARNING: Don't check your actual API key into GitHub
-// import {MOVIE_DB_API_KEY} from '../../api-keys';
-// const MOVIE_DB_BASE_URL = 'https://api.themoviedb.org/3';
-const MOVIE_DB_BASE_URL = 'http://localhost:5000/api'
+// const MOVIE_DB_BASE_URL = 'http://localhost:5000/api'
 
 const createMovieDbUrl = (relativeUrl, queryParams) => {
-  // let baseUrl = `${MOVIE_DB_BASE_URL}${relativeUrl}?api_key=bb11e63899b98af0b998882e7b4c58ff&language=en-US`;
-  let baseUrl = `${MOVIE_DB_BASE_URL}${relativeUrl}?test=1`;
+  let baseUrl = `${relativeUrl}?test=1`;
+  // let baseUrl = `${MOVIE_DB_BASE_URL}${relativeUrl}?test=1`;
   if (queryParams) {
     Object.keys(queryParams)
       .forEach(paramName => baseUrl += `&${paramName}=${queryParams[paramName]}`);
@@ -16,14 +14,14 @@ const createMovieDbUrl = (relativeUrl, queryParams) => {
 // WARNING: Must pass parameters like this: ({param1, param2})
 // for async action creator helper to work
 export const getTopMovies = async ({page}) => {
-  const fullUrl = createMovieDbUrl('/tvshows/discover/tv', {
+  const fullUrl = createMovieDbUrl('/api/tvshows/discover/tv', {
     page
   });
   return fetch(fullUrl);
 }
 
 export const searchMovies = async ({ page, query}) => {
-  const fullUrl = createMovieDbUrl('/search/movie', {
+  const fullUrl = createMovieDbUrl('/api/search/movie', {
     page,
     query
   });
@@ -31,6 +29,6 @@ export const searchMovies = async ({ page, query}) => {
 }
 
 export const getMovieDetails = async({movieId}) => {
-  const fullUrl = createMovieDbUrl(`/tvshows/tv/${movieId}`);
+  const fullUrl = createMovieDbUrl(`/api/tvshows/tv/${movieId}`);
   return fetch(fullUrl);
 }
